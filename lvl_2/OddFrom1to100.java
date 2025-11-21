38d067ed7dccb38a9d85560ef805d7bad3e30f14
vinay-kumar290 <krvinay290@gmail.com>
2025-11-21 21:03:02 +0530

    modify Even & Odd

--- lvl_2/OddFrom1to100.java
+++ lvl_2/OddFrom1to100.java
@@ -3,8 +3,13 @@ package lvl_2;
 public class OddFrom1to100 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		for (int i = 1; i <= 100; i++) {
+			
+			if(i % 2 != 0) {
+				
+				System.out.print(i + ", ");
+			}
+		}
 	}
-
 }
