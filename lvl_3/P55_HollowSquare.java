81d454f0651b51eac0729194b5a3d04dc6064167
vinay-kumar290 <krvinay290@gmail.com>
2025-11-29 18:48:09 +0530

    Solved upto Q55

--- lvl_3/P55_HollowSquare.java
+++ lvl_3/P55_HollowSquare.java
@@ -1,10 +1,35 @@
 package lvl_3;
+import java.util.Scanner;
 
 public class P55_HollowSquare {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter side of a square: ");
+		int side = sc.nextInt();
+		
+		if (side <= 1) {
+			System.out.println("Inavlid entry. Try again!");
+		}
+		else {
+			System.out.println("");
+			
+			for (int i = 0; i < side; i++) {
+				
+				for (int j = 0; j < side; j++) {
+					
+					if ((i == 0) || (i == side - 1) || (j == 0) || (j == side - 1)) {
+						System.out.print("*  ");
+					}
+					else {
+						System.out.print("   ");
+					}
+				}
+				System.out.println("");
+			}
+		}
+		sc.close();
 	}
-
 }
